526693b60d38fb4432e957a5f8265efd358d7db1
Kurucz <kuruczjanoshivatalos@gmail.com>
2023-01-24 19:01:57 +0100

    ora

---
+++ src/app.js
@@ -0,0 +1,7 @@
+const nevlista = document.querySelector("#nevlista");
+var nevTomb =["pista", "Karoly", "Mari", "Peti"];
+
+
+var li = document.createElement('li');
+li.textContent = "Valami";
+nevlista .appendChild(li);
